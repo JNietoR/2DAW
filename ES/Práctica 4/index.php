8d5b77c3978e9fa1480e0c2b8e9d27bd7b67e3a7
JNietoR <jorge.nieto35@gmail.com>
2023-10-31 20:33:20 +0100

    h1 del form

--- ES/Práctica 4/index.php
+++ ES/Práctica 4/index.php
@@ -11,7 +11,7 @@
 <body>
     <div class="h-screen flex flex-col justify-center items-center bg-blue-100">
         <h1 class="mb-6 text-3xl">
-            File menu
+            - FILE MENU -
         </h1>
         <div class="w-full sm:max-w-md py-4 px-6 bg-white shadow sm:rounded">
             <?php
