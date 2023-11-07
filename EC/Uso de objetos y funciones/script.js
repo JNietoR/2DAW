e7f6cffa9d5cbe04ca4dd42617f5e39ee5305da5
JNietoR <jorge.nieto35@gmail.com>
2023-11-07 21:06:27 +0100

    Uso de objetos y funciones, ejercicio 1, 2 y comienzo ejercicio 3

---
+++ EC/Uso de objetos y funciones/script.js
@@ -0,0 +1,60 @@
+/* 
+Crea un objeto TUTOR, el cual tendrá los campos: nombre, edad, DNI y titulo universitario.
+*/
+
+let tutor = {
+    nombre: 'Evelyn',
+    edad: 25,
+    dni: '24242424A',
+    titulo: 'Ingenieria Informatica'
+};
+
+/*
+Crea un objeto ASIGNATURA. De cada asignatura necesitamos saber su nombre, el curso al que pertenece (un número entero) y el número de horas totales. 
+A continuación crea una lista de, al menos, 4 objetos asignaturas diferentes.
+*/
+
+let entornoCliente = {
+    nombre: 'Desarrollo web en entorno Cliente',
+    curso: 2,
+    horas: 4
+};
+let entornoServidor = {
+    nombre: 'Desarrollo web en entorno Servidor',
+    curso: 2,
+    horas: 4
+};
+let diseñoWeb = {
+    nombre: 'Diseño de interfaces Web',
+    curso: 2,
+    horas: 4
+}
+let despliegeWeb = {
+    nombre: 'Despliege de aplicaciones web en entorno Servidor',
+    curso: 2,
+    horas: 4
+}
+
+let asignaturas = {
+    entornoCliente,
+    entornoServidor,
+    diseñoWeb,
+    despliegeWeb
+}
+
+/* 
+Haciendo uso del objeto TUTOR creado en el ejercicio 1 y de la lista de objetos ASIGNATURA creada en el
+ejercicio 2, define un objeto ALUMNO.
+Un alumno tendrá un nombre, una edad, el ciclo que estudia, el curso en el que está (un número entero), un
+tutor, una lista de asignaturas y una lista de nota media de cada asignatura. 
+*/
+
+let alumno = {
+    nombre: 'Jorge',
+    edad:32,
+    ciclo:'DAW',
+    tutor:tutor,
+    asignaturas:asignaturas,
+    
+}
+
