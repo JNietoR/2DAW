eeea8d00074216622065a3a66b7c2fa38ea2c3b2
JNietoR <jorge.nieto35@gmail.com>
2023-12-13 16:33:51 +0100

    prepared statement en el login

--- ES/Práctica9.BasesDeDatos/login.php
+++ ES/Práctica9.BasesDeDatos/login.php
@@ -1,38 +1,54 @@
 <?php
-//Comprueba si le has dado al submit del formulario login con metodo post
-if (isset($_POST['login'])) {
-    //almadena las variables del formulario login
+// Comprueba si se ha enviado el formulario de login mediante el método POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+    // Almacena las variables del formulario de login
     $user = $_POST["username"];
     $password = $_POST["password"];
 
-    //inicialización de las variables de la base de datos
+    // Variables de la base de datos
     $host = "localhost";
     $usuario = "root";
     $pass = "";
+    $db = "digimon";
 
-    //crea la conexion usando la dirección usuario y contraseña de la base de datos si no coinciden cierra la conexion
-    $conexion = mysqli_connect($host, $usuario, $pass) or die("Error de conexión");
-    //realiza una conexion a la base de datos llamada usuarios
-    mysqli_select_db($conexion, "digimon");
-    //query para comprobar que existe el usuario con su contraseña en la base de datos
-    $consultar = "SELECT * FROM users WHERE username='$user' AND password='$password'";
-    //realiza la query en la base de datos
-    $resultado = mysqli_query($conexion, $consultar);
-    //cierra la conexión despues de realizarla
-    mysqli_close($conexion);
+    // Intenta establecer la conexión a la base de datos
+    try {
+        $conexion = new mysqli($host, $usuario, $pass, $db);
 
-    //si encuentra una coincidencia inicia la sesión y crea las variables de sesión que vamos a utilizar
-    if (mysqli_num_rows($resultado) > 0) {
-        //inicia la sesión
-        session_start();
-        //inicializa las variables de los datos que deseamos guardar
-        $_SESSION['iniciada'] = true;
-        $_SESSION['user_id']= $user;
-        //redirecciona al index donde estan todas las funciones de los ficheros
-        header("Location: index.php"); 
-        //Si no ha encontrado coincidencia con el query sql redirecióna al index.html donde esta formulario de login
-    } else {
-        header("Location: index.html");
+        if ($conexion->connect_error) {
+            die("Error de conexión: " . $conexion->connect_error);
+        }
+
+        // Query con prepared statement para evitar inyecciones SQL
+        $consultar = "SELECT * FROM users WHERE username=? AND password=?";
+        $stmt = $conexion->prepare($consultar);
+
+        // Vincula los parámetros al statement
+        $stmt->bind_param("ss", $user, $password);
+
+        // Ejecuta la consulta
+        $stmt->execute();
+
+        // Obtiene el resultado de la consulta
+        $resultado = $stmt->get_result();
+
+        // Si encuentra una coincidencia, inicia la sesión y redirige al index.php
+        if ($resultado->num_rows > 0) {
+            session_start();
+            $_SESSION['iniciada'] = true;
+            $_SESSION['user_id'] = $user;
+            header("Location: index.php");
+        } else {
+            // Si no hay coincidencia, redirige al index.html
+            header("Location: index.html");
+        }
+    } catch (Exception $e) {
+        // En caso de error en la conexión o consulta
+        die("Error: " . $e->getMessage());
+    } finally {
+        // Cierra la conexión y el statement para mas seguridad
+        $stmt->close();
+        $conexion->close();
     }
 }
 ?>
