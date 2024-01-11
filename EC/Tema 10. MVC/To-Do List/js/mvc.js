ca438da94f99c3d1bcbb53e577f199ea6108865f
JNietoR <jorge.nieto35@gmail.com>
2024-01-11 19:57:46 +0100

    comentarios al código

--- EC/Tema 10. MVC/To-Do List/js/mvc.js
+++ EC/Tema 10. MVC/To-Do List/js/mvc.js
@@ -1,109 +1,109 @@
-// Model
-model = {
-    //iniciar array vacio para la lista de tareas
-    items: [] 
+// Modelo
+const model = {
+    // Inicializar array vacío para la lista de tareas
+    tasks: [] 
 }
 
-// View
-view = {
-    // Function para limpiar la lista
+// Vista
+const view = {
+    // Función para limpiar la lista
     clearList: function () {
-        var range = document.createRange()
+        const range = document.createRange()
         range.selectNodeContents(document.getElementById("list"));
         range.deleteContents();
     },
-    // funcion para renderizar la lista con todos sus cambios
+    // Función para renderizar la lista con todos sus cambios
     render: function () {
-        //primero se limpia la lista
+        // Primero se limpia la lista
         this.clearList();
 
-        if (model.items.length != 0) {
-            // for para imprimir la lista y sus iconos
-            list = document.getElementById("list");
-            for (var i = 0; i < model.items.length; i++) {
-                item = document.createElement("li");
-                span = document.createElement("span");
-                check = document.createElement("a");
-                cross = document.createElement("a");
-                iconCheck = document.createElement("i");
-                iconCross = document.createElement("i");
+        if (model.tasks.length !== 0) {
+            // For para imprimir la lista y sus iconos
+            const list = document.getElementById("list");
+            for (let i = 0; i < model.tasks.length; i++) {
+                const taskElement = document.createElement("li");
+                const taskTextElement = document.createElement("span");
+                const completeLinkElement = document.createElement("a");
+                const deleteLinkElement = document.createElement("a");
+                const completeIconElement = document.createElement("i");
+                const deleteIconElement = document.createElement("i");
 
-                // clases para elemento de la lista
-                item.className = "item";
-                span.className = "item-text";
-                check.className = "item-complete";
-                cross.className = "item-delete";
+                // Clases para elemento de la lista
+                taskElement.className = "item";
+                taskTextElement.className = "item-text";
+                completeLinkElement.className = "item-complete";
+                deleteLinkElement.className = "item-delete";
 
-                // cambiar los estilos si se completa la tarea tachandola
-                span.textContent = model.items[i].text;
-                if (model.items[i].completed) {
-                    span.setAttribute("style", "text-decoration: line-through; color: #bbb;")
+                // Cambiar los estilos si se completa la tarea tachándola
+                taskTextElement.textContent = model.tasks[i].text;
+                if (model.tasks[i].completed) {
+                    taskTextElement.setAttribute("style", "text-decoration: line-through; color: #bbb;")
                 }
 
-                // iconos para completar y eliminar tareas
-                iconCheck.setAttribute("class", "icon ion-md-checkmark");
-                iconCheck.setAttribute("data-id", i);
-                iconCross.setAttribute("class", "icon ion-md-trash");
-                iconCross.setAttribute("data-id", i);
+                // Iconos para completar y eliminar tareas
+                completeIconElement.setAttribute("class", "icon ion-md-checkmark");
+                completeIconElement.setAttribute("data-id", i);
+                deleteIconElement.setAttribute("class", "icon ion-md-trash");
+                deleteIconElement.setAttribute("data-id", i);
 
-                // eventos para completar y eliminar tareas
-                check.setAttribute("onclick", "controller.completeItem('" + i + "')");
-                cross.setAttribute("onclick", "controller.deleteItem('" + i + "')");
+                // Eventos para completar y eliminar tareas
+                completeLinkElement.setAttribute("onclick", "controller.completeTask('" + i + "')");
+                deleteLinkElement.setAttribute("onclick", "controller.deleteTask('" + i + "')");
 
-                // Agregar todos los elementos de una tarea, es decir su texto, y los iconos de eliminar y completar
-                check.appendChild(iconCheck);
-                cross.appendChild(iconCross);
-                item.appendChild(span);
-                item.appendChild(check);
-                item.appendChild(cross);
+                // Agregar todos los elementos de una tarea, es decir, su texto, y los iconos de eliminar y completar
+                completeLinkElement.appendChild(completeIconElement);
+                deleteLinkElement.appendChild(deleteIconElement);
+                taskElement.appendChild(taskTextElement);
+                taskElement.appendChild(completeLinkElement);
+                taskElement.appendChild(deleteLinkElement);
 
-                list.appendChild(item);
+                list.appendChild(taskElement);
             }
         }
     },
-    // Función para agergar una tarea al pulsar enter ya que no hemos agregado un submit
-    addItem: function (e) {
-        if ((e.code == "Enter") || (e.code == "NumpadEnter")) {
-            if (((document.getElementById("add-item").value != "") && (document.getElementById("add-item").value != " "))) {
-                item = document.getElementById("add-item").value;
-                controller.addItem(item);
+    // Función para agregar una tarea al presionar Enter, ya que no hemos agregado un submit
+    addItem: function (event) {
+        if ((event.code === "Enter") || (event.code === "NumpadEnter")) {
+            if (((document.getElementById("add-item").value !== "") && (document.getElementById("add-item").value !== " "))) {
+                const task = document.getElementById("add-item").value;
+                controller.addTask(task);
                 return false;
             }
         }
     },
 }
 
-// Controller
-controller = {
-    // inicializar el controlador de la aplicación
+// Controlador
+const controller = {
+    // Inicializar el controlador de la aplicación
     init: function () {
         view.render();
     },
     // Agregar una nueva tarea a la lista
-    addItem: function (item) {
-        list_item = { text: item, completed: false };
-        model.items.push(list_item);
+    addTask: function (task) {
+        const taskItem = { text: task, completed: false };
+        model.tasks.push(taskItem);
         document.getElementById("add-item").value = "";
-        //volver a renderizar despues de cada cambio
+        // Volver a renderizar después de cada cambio
         view.render();
     },
-    // cambiar entre completado y no completado
-    completeItem: function (item_index) {
-        model.items[item_index].completed = !model.items[item_index].completed;
-        //volver a renderizar despues de cada cambio
+    // Cambiar entre completado y no completado
+    completeTask: function (taskIndex) {
+        model.tasks[taskIndex].completed = !model.tasks[taskIndex].completed;
+        // Volver a renderizar después de cada cambio
         view.render();
     },
     // Eliminar elemento de la lista
-    deleteItem: function (item_index) {
-        model.items.splice(item_index, 1);
-        //volver a renderizar despues de cada cambio
+    deleteTask: function (taskIndex) {
+        model.tasks.splice(taskIndex, 1);
+        // Volver a renderizar después de cada cambio
         view.render();
     },
 }
 
-// inicializar el controlador
+// Inicializar el controlador
 controller.init();
-//elementos hardcodeados para ejemplo
-controller.addItem("Terminar DAW")
-controller.addItem("Salir vivo de DAW")
-controller.addItem("Terminar Baldur's Gate 3 con Álvaro y Jorge")
+// Elementos hardcodeados para ejemplo
+controller.addTask("Terminar DAW");
+controller.addTask("Salir vivo de DAW");
+controller.addTask("Terminar Baldur's Gate 3 con Álvaro y Jorge");
