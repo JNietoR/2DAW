1146bfce6f4839cc9bb014f555b3d65e196db8b1
JNietoR <jorge.nieto35@gmail.com>
2024-01-24 17:42:26 +0100

    clase jugador y enemigo

--- EC/Tema13.TypeScript/index.ts
+++ EC/Tema13.TypeScript/index.ts
@@ -1,9 +1,3 @@
-function calcularFuerzaInicial() {
-    let min = Math.ceil(1);
-    let max = Math.floor(10);
-    return Math.floor(Math.random() * (10 - 1) + 1);
-}
-
 
 class Jugador {
     nombre: string;
@@ -14,9 +8,8 @@ class Jugador {
     constructor(nombre: string) {
         this.nombre = nombre;
         this.puntos_salud = 20;
-        this.puntos_ataque = calcularFuerzaInicial();
+        this.puntos_ataque = 0;
         this.dinero = 2;
-
     }
     public getNombre() {
         return this.nombre;
@@ -45,10 +38,54 @@ class Jugador {
     public setDinero(dinero: number) {
         this.dinero = dinero;
     }
-
-
+    public calcularFuerzaInicial() {
+        return Math.floor(Math.random() * (10 - 1) + 1);
+    }
+    toString(): string {
+        return `Jugador: ${this.nombre}, HP: ${this.puntos_salud}, ATQ: ${this.puntos_ataque}, GOLD: ${this.dinero} }`;
+    }
 
 }
 class Enemigo {
+    nombre: string;
+    puntos_ataque: number;
 
+    constructor(nombre: string) {
+        this.nombre = nombre;
+        this.puntos_ataque = 0;
+        // puntos de vida ? para matarlos
+
+    }
+    public getNombre() {
+        return this.nombre;
+    }
+
+    public setNombre(nombre: string) {
+        this.nombre = nombre;
+    }
+    public getPuntos_ataque() {
+        return this.puntos_ataque;
+    }
+
+    public setPuntos_ataque(puntos_ataque: number) {
+        this.puntos_ataque = puntos_ataque;
+    }
+    public calcularFuerzaEnemigo() {
+        return Math.floor(Math.random() * (10 - 1) + 1);
+    }
+    
+    public soltarDinero() {
+        return Math.floor(Math.random() * (3 - 1) + 1);
+    }
+}
+class Partida{
+    migue= new Enemigo("Migue Roso");
+    ramirez= new Enemigo("Ramirez");
+    pablo= new Enemigo("Pablo Motos");
+
+    enemigos: Enemigo[] = [this.migue,this.ramirez,this.pablo];
+
+
+   historia: string =" aaa";
+    
 }
