d768a5804798ba884adaa952dda02e94fd0678c7
JNietoR <jorge.nieto35@gmail.com>
2024-02-08 23:03:44 +0100

    empezar estructura basica

--- EC/cosmere/src/App.vue
+++ EC/cosmere/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
+  <div class="text-white text-center">
+
+    <h1 class="m-auto my-8 text-4xl shadow-2xl shadow-black w-96">Sagas del Cosmere</h1>
+    <ul class="text-left">
+      <li>Archivo de las tormentas</li>
+      <li>Mistborn</li>
+      <li>Novelas Secretas</li>
+    </ul>
 
   </div>
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
